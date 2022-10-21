6039b23b41790a7232c0baa67bb7a5f41e93a610
ayrton <ayrton.parkinson1@gmail.com>
2022-10-21 14:15:12 -0400

    Add useEffect to fetch and set GameSelection menu

--- client/src/components/GameSelection.js
+++ client/src/components/GameSelection.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import GamePreview from './GamePreview';
 
 function GameSelection() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/games', {
+      mode: 'cors'
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      setGames(data);
+    })
+  }, []);
+
   return (
-    <div>Game Selection Time!!!</div>
+    <div>
+      <div>Game Selection Time!!!</div>
+      {games.map((game) => (<GamePreview key={game.id} game={game} />))}
+    </div>
   );
 }
 
