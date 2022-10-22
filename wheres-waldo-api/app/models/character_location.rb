532d1913b50da0c718319e862120c606dee337c1
ayrton <ayrton.parkinson1@gmail.com>
2022-10-22 11:16:51 -0400

    Add #correct_coordinates? to evaluate whether CharacterLocation includes coordinates

--- wheres-waldo-api/app/models/character_location.rb
+++ wheres-waldo-api/app/models/character_location.rb
@@ -1,4 +1,11 @@
 class CharacterLocation < ApplicationRecord
   belongs_to :game
   belongs_to :character
+
+  def correct_coordinates?(x_val, y_val)
+    return false unless x_val.between?(x_coordinates[0], x_coordinates[1])
+    return false unless y_val.between?(y_coordinates[0], y_coordinates[1])
+
+    true
+  end
 end
