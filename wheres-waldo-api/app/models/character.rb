e02b278900a1977457a53ba7cdd1cbf530ac0101
ayrton <ayrton.parkinson1@gmail.com>
2022-10-22 09:38:39 -0400

    Add custom #as_json

--- wheres-waldo-api/app/models/character.rb
+++ wheres-waldo-api/app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ApplicationRecord
   has_many :character_locations
+
+  def as_json(options = {})
+    options[:only] = %i[id name image_name] unless options[:only]
+    super(options)
+  end
 end
--- wheres-waldo-api/app/models/game.rb
+++ wheres-waldo-api/app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ApplicationRecord
   has_many :character_locations
   has_many :characters, through: :character_locations
+
+  def as_json(options = {})
+    options[:only] = %i[id name image_name] unless options[:only]
+    super(options)
+  end
 end
