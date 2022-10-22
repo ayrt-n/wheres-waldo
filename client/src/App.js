ce6adda05df75d617aed35b8b5955f33e5a57252
ayrton <ayrton.parkinson1@gmail.com>
2022-10-22 09:58:19 -0400

    Rename parameter to gameId

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<GameSelection />} />
         <Route path="/games" element={<GameSelection />} />
-        <Route path="/games/:id" element={<Game />} />
+        <Route path="/games/:gameId" element={<Game />} />
       </Routes>
     </BrowserRouter>
   );
