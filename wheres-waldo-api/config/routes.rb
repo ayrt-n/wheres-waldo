fe49ae225020dbb676b98a9315cdae02598b15f7
ayrton <ayrton.parkinson1@gmail.com>
2022-10-22 11:16:02 -0400

    Change get /character_locations to post to accept form params

--- wheres-waldo-api/config/routes.rb
+++ wheres-waldo-api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :games, only: %i[index show]
+      post '/character_locations', to: 'character_locations#verify_location', as: 'character_locations'
     end
   end
 end
