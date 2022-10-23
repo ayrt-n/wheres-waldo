b95ed7eba736a75d594fa769b2dca9df6d47be64
ayrton <ayrton.parkinson1@gmail.com>
2022-10-23 09:49:53 -0400

    Add #create_verification_response to evaluate guesses

--- wheres-waldo-api/app/models/character_location.rb
+++ wheres-waldo-api/app/models/character_location.rb
@@ -2,6 +2,25 @@ class CharacterLocation < ApplicationRecord
   belongs_to :game
   belongs_to :character
 
+  def create_verification_response(x_val, y_val)
+    response = {
+      correct: correct_coordinates?(x_val, y_val),
+      details: {}
+    }
+
+    return response unless response[:correct]
+
+    response[:details] = {
+      character_id: character_id,
+      x_coordinates: x_coordinates,
+      y_coordinates: y_coordinates
+    }
+
+    response
+  end
+
+  private
+
   def correct_coordinates?(x_val, y_val)
     return false unless x_val.between?(x_coordinates[0], x_coordinates[1])
     return false unless y_val.between?(y_coordinates[0], y_coordinates[1])
