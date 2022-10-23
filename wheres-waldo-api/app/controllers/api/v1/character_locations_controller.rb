e95e78e6ca2c6765488533d1c55285921fe2dba6
ayrton <ayrton.parkinson1@gmail.com>
2022-10-23 09:50:19 -0400

    Refactor controller to move logic to model

--- wheres-waldo-api/app/controllers/api/v1/character_locations_controller.rb
+++ wheres-waldo-api/app/controllers/api/v1/character_locations_controller.rb
@@ -3,9 +3,7 @@ module Api
     class CharacterLocationsController < ApplicationController
       def verify_location
         @location = CharacterLocation.find_by(game_id: params[:game_id], character_id: params[:character_id])
-        @response = {
-          response: @location.correct_coordinates?(params[:x].to_i, params[:y].to_i)
-        }
+        @response = @location.create_verification_response(params[:x].to_i, params[:y].to_i)
 
         render json: @response
       end
