7a0258cd547b7dc57e8d92668ef424474183a246
ayrton <ayrton.parkinson1@gmail.com>
2022-10-24 07:03:57 -0400

    Add routes for leaderboards

--- wheres-waldo-api/config/routes.rb
+++ wheres-waldo-api/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :games, only: %i[index show]
+      resources :games, only: %i[index show] do
+        resources :leaderboards, only: %i[index create]
+      end
       post '/character_locations', to: 'character_locations#verify_location', as: 'character_locations'
     end
   end
