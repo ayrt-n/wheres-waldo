da48185122b6cc68542b6f0b659842c6390a1b60
ayrton <ayrton.parkinson1@gmail.com>
2022-10-24 06:13:46 -0400

    Fix x, y coordinate naming

--- client/src/components/Marker.js
+++ client/src/components/Marker.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function Marker({ coordinates }) {
-  const { x_coordinates, y_coordinates } = coordinates
-  const width = x_coordinates[1] - x_coordinates[0];
-  const height = y_coordinates[1] - y_coordinates[0];
+  const { x, y } = coordinates
+  const width = x[1] - x[0];
+  const height = y[1] - y[0];
 
   const markerStyles = {
-    left: x_coordinates[0] + 'px',
-    top: y_coordinates[0] + 'px',
+    left: x[0] + 'px',
+    top: y[0] + 'px',
     width: width + 'px',
     height: height + 'px',
   }
