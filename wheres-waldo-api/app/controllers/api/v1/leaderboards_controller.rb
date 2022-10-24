1fe8520e5b0e95378e4adc887ac43fc8010771a9
ayrton <ayrton.parkinson1@gmail.com>
2022-10-24 07:04:35 -0400

    Create Leaderboards controller with #index and #show

---
+++ wheres-waldo-api/app/controllers/api/v1/leaderboards_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    class LeaderboardsController < ApplicationController
+      def index
+        @leaderboards = Score.limit(5).order(ellapsed_time: :asc)
+      end
+
+      def create
+        @leaderboard = Score.create(score_params)
+      end
+
+      private
+
+      def score_params
+        params.require(:score).permit(:name, :ellapsed_time)
+      end
+    end
+  end
+end
