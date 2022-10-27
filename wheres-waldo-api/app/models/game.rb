1d3426053a5f27d1721e388d404518513d91bb25
ayrton <ayrton.parkinson1@gmail.com>
2022-10-27 07:20:21 -0400

    Add Score-Game association to models

--- wheres-waldo-api/app/models/game.rb
+++ wheres-waldo-api/app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   has_many :character_locations
   has_many :characters, through: :character_locations
+  has_many :scores
 
   def as_json(options = {})
     options[:only] = %i[id name image_name] unless options[:only]
